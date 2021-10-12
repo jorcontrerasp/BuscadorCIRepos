6db925fd6cc63a5792f3601a89dfbe36789ca0b7
Jorge Contreras Padilla <77851630+jorcontrerasp@users.noreply.github.com>
2021-10-12 19:17:39 +0200

    API de GitHUb
    Uso de la API de GitHub

---
+++ auxiliares.py
@@ -0,0 +1,22 @@
+#AQUÍ se definirán las funciones auxiliares del programa.
+
+#Importamos las librerías necesarias.
+import pickle
+
+def generarPickle(nombreFichero, listaRepositorios):
+    print("Generando fichero pickle...")
+    with open(nombreFichero, 'wb') as f:
+        pickle.dump(listaRepositorios, f)
+    print("Fichero " + nombreFichero + " generado")
+
+def cargarRepositorios(fichero):
+    print("Cargando repositorios...")
+    with open(fichero, 'rb') as f:
+        repositories = pickle.load(f)
+    return repositories
+
+def imprimirListaRepositorios(repositorios):
+    print("Imprimiendo lista de repositorios...")
+    for project in repositorios:
+        project_name = project.full_name.split("/")[1]
+        print(project.full_name)
--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
+#TFG Grado en Ingeniería de Computadores
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#Importamos las librerías necesarias.
+from github import Github
+import random
+import auxiliares as aux
 
+print("Iniciando proceso...")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+try:
+    # Generamos un token para consultar la API de GitHub a través de la librería.
+    user = "jorcontrerasp"
+    token = "AAA"
+    g = Github(user, token)
 
+    query = """
+        language:java 
+        stars:>=500 
+        forks:>=300 
+        created:<2015-01-01 
+        pushed:>2020-01-01
+        archived:false
+        is:public
+    """
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    generator = g.search_repositories(query=query)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Convertimos el generador en una lista de repositorios.
+    repositories = list(generator)
+
+    # Guardamos la información de los repositorios recuperados en un archivo binario de Python.
+    fRepos = "repos.pickle"
+    aux.generarPickle(fRepos, repositories)
+    repositories = aux.cargarRepositorios(fRepos)
+
+    # Filtramos por el número de COMMITS.
+    boFiltrarCommits = False
+
+    MAX_COMMITS = 10000
+    MIN_COMMITS = 1000
+    filteredRepos = []
+
+    if boFiltrarCommits:
+        for repo in repositories:
+            commits = repo.get_commits().totalCount
+            if commits >= MIN_COMMITS and commits <= MAX_COMMITS:
+                filteredRepos.append(repo)
+    else:
+        for repo in repositories:
+            filteredRepos.append(repo)
+
+    # Seleccionamos N repositorios de manera aleatoria:
+    lRandom = []
+    while len(lRandom) < 20:
+        item = random.choice(filteredRepos)
+        if item not in lRandom:
+            lRandom.append(item)
+
+    # Imprimimos la lista de repositorios
+    aux.imprimirListaRepositorios(lRandom)
+
+    print("Proceso finalizado.")
+
+except:
+    print("Se ha producido un ERROR inesperado.")
+    raise
+    # FIN
