0a0bb566a2a84fe76693a31cdbcfd45af7e09dc3
Jorge Contreras Padilla <77851630+jorcontrerasp@users.noreply.github.com>
2021-11-29 22:39:33 +0100

    YML y YAML examples

---
+++ yaml_parser.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import io
+import yaml
+
+data = yaml.safe_load(open('yml_example_files/defaults.yaml'))
+url = data['url']
+
+
+data2 = yaml.safe_load(open('yml_example_files/.gitlab-ci.yml'))
+stages = data2['stages']
+job1 = data2['test-code-job2']
+job1_script = job1['script']
+#for s in job1_script:
+#    print(s)
+
+data3 = yaml.safe_load(open('gitlabCI_yml_example_files/apple_turicreate_gitlab-ci.yml'))
+print(data3)
